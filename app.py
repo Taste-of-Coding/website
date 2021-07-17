e1cafe58e5f896744f9ef2eeee991b4a1552175b
casskycity <castieltong@gmail.com>
2021-07-17 21:57:43 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import smtplib, ssl
+from email.message import EmailMessage
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("main.html")
+
+
+@app.route("/enquiry", methods=["POST", "GET"])
+def email_enquiry():
+
+    if request.method == "POST":
+        if "recipient email" in request.form and "firstname" in request.form:
+            recipient_email = request.form["recipient email"]
+            firstname = request.form["firstname"]
+            subject = request.form["subject"]
+            context = request.form["context"]
+            smtp_server = "smtp.gmail.com"
+            sender_email = "luviertcc@gmail.com"
+            password = "fsxdgqrcavgcsfyc"
+
+            # setup email message
+            msg = EmailMessage()
+            msg.set_content(context)
+            msg['Subject'] = subject
+
+            # setup smtp server and send email
+            smtp_context = ssl.create_default_context()
+            with smtplib.SMTP(smtp_server, 587) as server:
+                server.ehlo()
+                server.starttls(context=smtp_context)
+                server.ehlo()
+                server.login(sender_email, password)
+                server.sendmail(sender_email, recipient_email, msg.as_string())
+
+            return "200"
+
+        else:
+            return "recipient email or firstname not found."
+
+    else:
+        return render_template("email.html")
+
+
+# if __name__ == "__main__":
+#     app.run("0.0.0.0", port=2021, debug=True)
